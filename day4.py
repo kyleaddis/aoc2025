1b96bf406e6f5c86f8631bd41ee4756b536e303d
kyleaddis <kyle.a.addis@gmail.com>
2025-12-04 10:48:50 -0500

    Day 4 - Part 2 Done Moved input files

--- day4.py
+++ day4.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import sys
 
 if len(sys.argv) != 2:
@@ -8,11 +9,12 @@ with open(sys.argv[1]) as f:
     grid = f.read()
 
 grid = grid.split()
+grid_list = [list(g) for g in list(grid)]
 
 
 def roll_accesible(grid, x, y):
     if grid[x][y] != "@":
-        return 0
+        return 0, grid[x][y]
     rows = len(grid)
     cols = len(grid[0])
     count = -1
@@ -22,14 +24,28 @@ def roll_accesible(grid, x, y):
                 if grid[r][c] == "@":
                     count += 1
                     if count > 3:
-                        return 0
+                        return 0, "@"
 
-    return 1
+    return 1, "X"
 
 
 result = []
 for r in range(len(grid)):
     for c in range(len(grid[0])):
-        result.append(roll_accesible(grid, r, c))
+        result.append(roll_accesible(grid, r, c)[0])
 
-print(sum(result))
+print(f"Part 1: {sum(result)}")
+
+result = []
+new_grid = deepcopy(grid_list)
+while True:
+    for r in range(len(grid_list)):
+        for c in range(len(grid_list[0])):
+            val, symbol = roll_accesible(grid_list, r, c)
+            grid_list[r][c] = symbol
+            result.append(val)
+    if new_grid == grid_list:
+        break
+    new_grid = deepcopy(grid_list)
+
+print(f"Part 2: {sum(result)}")
